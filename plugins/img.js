9afc1dfc8cda810157ae5c1821728fdfa3a0897a
HypBots <hypbots@gmail.com>
2025-06-30 14:17:57 +0530

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 caption: `
 🌟 *Image ${i + 1} from your search!* 🌟
         *Enjoy these images! 📸*
-> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*
+> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʀᴀᴠɪɴᴅᴜ ʟᴀᴋꜱʜɪᴛʜᴀ*
 `
 }, { quoted: mek });
 }
