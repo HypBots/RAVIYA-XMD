310a2b826e5798533e7f5110945a1081151114b4
HypBots <hypbots@gmail.com>
2025-06-30 14:28:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG : process.env.ALIVE_IMG || "https://pomf2.lain.la/f/uzu4feg.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : Bot Name",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'RAVIYA-XMD=DsQTHL4I#VOkD-B3rOCXqm5jk6gDUzQkZWiC6DiGGHsDxv9Mefgg' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.' ,
+SUDO: process.env.SUDO === undefined ? '94772854872' : process.env.SUDO,
+MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
 };
